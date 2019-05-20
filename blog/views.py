dd1858122f84fa9c09d02458ec170718ef5e827d
Supakrit <60070097@kmitl.ac.th>
2019-05-20 21:22:26 +0700

    Connect to database

--- blog/views.py
+++ blog/views.py
@@ -1,37 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'DsdlTon',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 5, 2019'  
-    },
-    {
-        'author': 'Fon',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 25, 2019'  
-    },
-    
-]
-
-abouts = [
-    {
-        'title': 'About'
-    }
-]
-
+# from django.http import HttpResponse
+from .models import Post
 
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 def about(request):
-    context = {
-        'title': abouts
-    }
-    return render(request, 'blog/about.html', context)
+    return render(request, 'blog/about.html', {'title': 'About'})
--- django_for_final/settings.py
+++ django_for_final/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_for_final.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_for_final',
+        'USER': 'root',
+        'PASSWORD': 'ton30217',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
